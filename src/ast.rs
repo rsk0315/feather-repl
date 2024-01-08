aa0e0de3ca9dc688d8d6ff07b9f2f93480f84274
rsk0315 <rsk0315@icloud.com>
2024-01-08 11:43:44 +0900

    spaces and positions

--- src/ast.rs
+++ src/ast.rs
@@ -1,9 +1,13 @@
+use std::ops::Range;
+
+use combine::stream::PointerOffset;
+
 #[derive(Debug)]
 pub enum Expr {
-    Literal(String),
-    Mul(Box<Expr>, Box<Expr>),
-    Div(Box<Expr>, Box<Expr>),
-    Add(Box<Expr>, Box<Expr>),
-    Sub(Box<Expr>, Box<Expr>),
-    Neg(Box<Expr>),
+    Literal(String, Range<PointerOffset<str>>),
+    Mul(Box<Expr>, Box<Expr>, Range<PointerOffset<str>>),
+    Div(Box<Expr>, Box<Expr>, Range<PointerOffset<str>>),
+    Add(Box<Expr>, Box<Expr>, Range<PointerOffset<str>>),
+    Sub(Box<Expr>, Box<Expr>, Range<PointerOffset<str>>),
+    Neg(Box<Expr>, Range<PointerOffset<str>>),
 }
